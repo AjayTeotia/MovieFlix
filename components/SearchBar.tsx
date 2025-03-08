d36347d66a886414097681d7e08975ef43fc476c
AjayTeotia <teotiaajay20@gmail.com>
2025-03-08 13:13:53 +0530

    home-basic-ui

---
+++ components/SearchBar.tsx
@@ -0,0 +1,32 @@
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
+
+interface Props {
+    placeholder: string;
+    value?: string;
+    onChangeText?: (text: string) => void;
+    onPress?: () => void;
+}
+
+export default function SearchBar({ placeholder, value, onChangeText, onPress }: Props) {
+    return (
+        <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+            <Image
+                source={icons.search}
+                className='size-5'
+                resizeMode='contain'
+                tintColor={"#AB8BFF"}
+            />
+
+            <TextInput
+                onPress={onPress}
+                placeholder={placeholder}
+                value={value}
+                onChangeText={onChangeText}
+                className="flex-1 ml-2 text-white"
+                placeholderTextColor="#A8B5DB"
+            />
+        </View>
+    )
+}
